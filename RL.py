216711cdb8efdd8935da463527ca3a8caba2f80d
babamama1025 <babamama711025@gmail.com>
2017-04-24 20:07:12 -0500

    Add files via upload

--- RL.py
+++ RL.py
@@ -23,56 +23,63 @@ def readTraingingSettings():
     y_set=lines[5].split() 
     minG=lines[6].split() # minimun green time
     minG=map(int,minG) # string to integer
-    
-    return mapName[0],TCid[0],laneSet,laneSigDigit,phase_set,y_set,minG
+    lostTime=lines[7].split() # lost time [yellow, red]
+    lostTime=map(int,lostTime) # string to integer
+    return mapName[0],TCid[0],laneSet,laneSigDigit,phase_set,y_set,minG,lostTime[0],lostTime[1]
 
 
-def calOBJ():
+def calOBJ(laneSet,s_q,s_er):
     # weighted average of N_q and N_er
-    return -1*( sum(s_q) + np.dot(s_q,s_er)) # assume weight =1
+    delay=sum(s_q)
+    penal=0 # penaty value
+    for er in s_er:
+        exced=max(er-_c.tol_er,0) # the time over tolerance level
+        if exced>0:
+            penal=+ exced*_c.pen_rate
+    return -1*( delay + penal ) 
 
 def choseAction():
     # given state, return an action with e-greedy policy
     # decent e rate, e = max (min_p,max_p-t/max_ite)
     # thus e = min_p when t =(maxp-minp)*maxite
     if _c.b_train:
-        e = max(_c.e_min_p,_c.e_max_p-t/_c.e_max_ite)
+        e = max(_c.e_min_p,_c.e_max_p-t * (_c.e_max_p-_c.e_min_p) /_c.e_max_ite)
     else:
         e=0 # deterministic
-    new_p=e_greedy(e) # get new phase index
-    return new_p,e
+    new_p,Q=e_greedy(e) # get new phase index
+    return Q,new_p,e
 
 def e_greedy(e):
     # episolon greedy
     # return phase number (0,1,...,P-1)
-
+    # Snew * w in R^│A│ each is the Q value under (s,a)
+    W=np.c_[Wc,We[:,curPhase]] # weight representing changing or extending current phase
+    Q=np.dot(W.T,Snew)
+    Q[curPhase]=-float("inf")  # can't compare to the curent phase
     if rd.random()<e: # random pick
         a=range(len(A_set))
+        a.pop(curPhase)
+        
         print "random pick."
-        return rd.choice(a)
+        return rd.choice(a),Q
     else: # find the action that has the minimum cost
-        # Snew * w in R^│A│ each is the Q value under (s,a)
-        Q=np.dot(w.T,Snew)
         print "Q(Snew) = ",Q
-        return np.argmax(Q)
+        return np.argmax(Q),Q
     
 def updatePhase(curPhase,g_elps):
     # add yello and all-red phase to Plist 
     ar='r'*len(phase_set[0]) 
-    y=2 # yello 
-    r=1 # all red
-    if not Anew: # Anew == 0 means phase changing
-        for i in range(y):
+    if Anew < N_p: # Anew < Np means phase changing
+        for i in range(ls_y): # lost time yellow
             Plist.append(y_set[curPhase])
-        for i in range(r):
+        for i in range(ls_r): # lost time red
             Plist.append(ar)
-        curPhase+=1
-        curPhase = curPhase % len(phase_set)
+        curPhase = Anew
         Plist.append(phase_set[curPhase])
         g_elps=0
     return curPhase,Plist,g_elps
 
-def updateS():
+def updateS(s_q,s_er,s_eg):
     #update current queue length and elapsed time
     # get signal state
     sig_stat=traci.trafficlights.getRedYellowGreenState(TCid)
@@ -91,104 +98,140 @@ def updateS():
     return s_q,s_er,s_eg
 
 def updateW():
-#    inc=R + beta*np.dot(S_new_a,w_a)-np.dot(S_a,w_a)
-#    print "inc=",inc
-    if _c.updateScheme=="Q":
-        inc=R + _c.beta*np.dot(Snew,w[:,e_greedy(0)])-np.dot(S,w[:,A])
-    elif _c.updateScheme=="SARSA":
-        inc=R + _c.beta*np.dot(Snew,w[:,Anew])-np.dot(S,w[:,A])
-    w[:,A]=w[:,A]+_c.alpha * inc * S
-#    w[:,A]=w[:,A]+alpha * inc * S.reshape(len(S),1)
-#    w = w + alpha*(R + beta*w*Snew-w*S)*S
-    return w
+    Qact=e_greedy(0)
+    if Qact==N_p:
+       Qw=copy.copy(We)
+    else:
+       Qw=copy.copy(Wc)
+       
+    # update learning rate
+    alpha =max(_c.alpha_min,_c.alpha_max - t * (_c.alpha_max - _c.alpha_min)/_c.alpha_maxite)
+    print "Learning rate = ", alpha
+    if A==N_p and Anew==N_p: # (extend, extend) update We[:,curPhase] using We[:,curPhase] We[:,curPhase]
+        if _c.updateScheme=="Q":
+            inc=R + _c.beta * np.dot(Snew,Qw[:,curPhase]) - np.dot(S,We[:,curPhase]) 
+        elif _c.updateScheme=="SARSA":
+            inc=R + _c.beta * np.dot(Snew,We[:,curPhase]) - np.dot(S,We[:,curPhase]) 
+        We[:,curPhase]=We[:,curPhase] + alpha * inc * S
+    elif A==N_p and Anew < N_p: # (extend, change) update We[:,lastPhase] using Wc[:,curPhase] We[:,lastPhase]
+        if _c.updateScheme=="Q":
+            inc=R + _c.beta * np.dot(Snew,Qw[:,curPhase]) - np.dot(S,We[:,lastPhase]) 
+        elif _c.updateScheme=="SARSA":
+            inc=R + _c.beta * np.dot(Snew,Wc[:,curPhase]) - np.dot(S,We[:,lastPhase]) 
+        We[:,lastPhase]=We[:,lastPhase] + alpha * inc * S
+    elif A < N_p and Anew==N_p: # (change, extend) update Wc[:,curPhase] using We[:,curPhase] Wc[:,curPhase]
+        if _c.updateScheme=="Q":
+            inc=R + _c.beta * np.dot(Snew,Qw[:,curPhase]) - np.dot(S,Wc[:,curPhase]) 
+        elif _c.updateScheme=="SARSA":
+            inc=R + _c.beta * np.dot(Snew,We[:,curPhase]) - np.dot(S,Wc[:,curPhase]) 
+        Wc[:,curPhase]=Wc[:,curPhase] + alpha * inc * S
+    elif A < N_p and Anew < N_p: # (change, change) update Wc[:,lastPhase] using Wc[:,curPhase] Wc[:,lastPhase]
+        if _c.updateScheme=="Q":
+            inc=R + _c.beta * np.dot(Snew,Qw[:,curPhase]) - np.dot(S,Wc[:,lastPhase]) 
+        elif _c.updateScheme=="SARSA":
+            inc=R + _c.beta * np.dot(Snew,Wc[:,curPhase]) - np.dot(S,Wc[:,lastPhase]) 
+        Wc[:,lastPhase]=Wc[:,lastPhase] + alpha * inc * S
+        
+    return Wc,We
 
 if __name__ == "__main__":
-    
-#     # Network settings
+    # Read Network settings
     start = timeit.default_timer()
-    mapName,TCid,laneSet,laneSigDigit,phase_set,y_set,minG=readTraingingSettings()
+    mapName,TCid,laneSet,laneSigDigit,phase_set,y_set,minG,ls_y,ls_r=readTraingingSettings()
     passable_sig=['g','G','y'] # signals that resets elapsed time
-#   
-#    # Variable initialization
-    A_set=[0,1] # stop current phase or extend it
-    s_q=np.zeros(len(laneSet)) # number of stopping veh on each lane
-    s_er=np.zeros(len(laneSet)) # elapsed red time of each lane
-    s_eg=np.zeros(len(laneSet)) # elapsed green time of each lane, size L
-    perf_lane=np.zeros(len(laneSet)) # number of stopped vehicle through simulation period (T*L matrix)
-    w=np.zeros((len(laneSet)+len(phase_set),len(A_set))) # parameters of linear approximation, (L+P)*A matrix. P is one-hot
+    N_p=len(phase_set) # number of phases
+    N_l=len(laneSet) # number of lanes
     
-    if not _c.b_train: # load existing training result and start to simulate
-        w=np.load('training_result')
-    # A Q(s,a) value can be estimated by w[:,a]*[s_q]
-    if _c.b_outrlt and _c.b_train:
-        frlt = open('training_result.txt', 'w')   
-
-    A=0 # action (extend or stop the current phase)
-    Anew=A # new action (0 or 1)
+    # Variable initialization
+    A_set=range(N_p+1) # [switch to a phase (phase index), extend current phase (last column)]
+    We=np.zeros((N_l*2,N_p)) # weight of extending current phase  
+    Wc=np.zeros((N_l*2,N_p)) # weight of changing to otehr phase  
+    s_q=np.zeros(N_l) # number of stopping veh on each lane
+    s_er=np.zeros(N_l) # elapsed red time of each lane
+    s_eg=np.zeros(N_l) # elapsed green time of each lane, size L
+    perf_lane=np.zeros(N_l) # number of stopped vehicle through simulation period (T*L matrix)
+    S=np.append(s_q,s_er) # state (N_l*2): [# stopped veh of eah lane, red elapsed sec for each lane]
+    Snew=copy.copy(S)
+    Q=np.zeros(N_p+1)
+    Qnew=copy.copy(Q)
+    A=0 # action
+    Anew=A # new action
+    
+    # Assign initial values
     Plist=[] # list of phases storing y_set and all red, will pop through time if the list is not empty
     t=0 # step
+    g_elps=0 # start decision after min green is over
+    upd_dur=0 # update duration (time interval between actions)
     R=0 # total delay
     D=0 # departure veh
     curPhase=0 # current phase
-    g_elps=0
+    lastPhase=0
     
-    # initial state = [s_q,P_onehot]
-    S=np.append(s_q,np.zeros(len(phase_set)))
-    S[len(laneSet)+curPhase]=1 # initial phase =1 others =0 
-    Snew=copy.copy(S)
-    
-    # Start simulation
+    # Open log files
+    fout = open('log-W.txt', 'w')   
+    fsar_out = open('log-SAR.txt', 'w')   
+    # Start SUMO
     if _c.b_train:
 #         traci.start(["sumo", "-c", mapName+".sumo.cfg"])
          traci.start(["sumo-gui", "-c", mapName+".sumo.cfg","--random"])
     else:
          traci.start(["sumo-gui", "-c", mapName+".sumo.cfg","--random"])
-    try:
-        while t<_c.T:
-             # get current simulation time
-             ct=traci.simulation.getCurrentTime()/1000
-             # Update state
-             s_q,s_er,s_eg=updateS() # since Snew=s_q, the func updates both
-             R=R+calOBJ()
+         
+    # Start simulation         
+    while t<_c.T:
+         upd_dur+=1
+         # get current simulation time
+         ct=traci.simulation.getCurrentTime()/1000
+         ######## Update state
+         s_q,s_er,s_eg=updateS(s_q,s_er,s_eg) # since Snew=s_q, the func updates both
+         R=R+calOBJ(laneSet,s_q,s_er)
+         
+         # still in phase transition(lost time), pop up a plan index each time
+         if len(Plist): 
+             traci.trafficlights.setRedYellowGreenState(TCid,Plist.pop(0))
              
-             # still in phase transition(lost time), pop up a plan index each time
-             if len(Plist)>0: 
-                 traci.trafficlights.setRedYellowGreenState(TCid,Plist.pop(0))
+         ######## take action and update weight
+         if t%_c.updt_freq==0 and g_elps>=minG[curPhase]+ls_y+ls_r:
+             ######## Update State
+             Snew=np.append(s_q,s_er) # new state
+             R=R/upd_dur # average stopping veh + penalty
+             print "\n\nt = ",ct,"\nG_elps = ",g_elps,"\nA = ",A,"\nR = ",R
              
-             # take action and update weight
-             if t%_c.updt_freq==0 and g_elps>minG[curPhase]:
-                 Snew=np.append(s_q,np.zeros(len(phase_set))) # new state
-                 Snew[len(laneSet)+curPhase]=1 # current state =1 others =0 
-                 print "t = ",ct,"\nS = ",S,"\nA = ",A,"\nG_elps = ",g_elps,"\nR = ",R,"\nSnew = ",Snew
-                 # Choose an action Anew
-                 Anew,e=choseAction()
-                 curPhase,Plist,g_elps=updatePhase(curPhase,g_elps)
-                 # Update w
-                 if _c.b_train:
-                     w=updateW()
-                     
-                 print "Anew (e = ",e,") = ",Anew,"\nw = \n",w,"\n\n"
-                 S=copy.copy(Snew)
-                 A=Anew
-                 R=0
-                 D=0
-                 
-             perf_lane=np.vstack((perf_lane,s_q))
-             t+=1
+             ######## Choose an action
+             Qnew,Anew,e=choseAction()
+             print "Anew (e = ",e,") = ",Anew
+             curPhase,Plist,g_elps=updatePhase(curPhase,g_elps)    
+             # output S,A,R etc
+             fsar_out.write(str(t) + ";" + str(S.tolist())[1:-1]+";"+str(Q)[1:-1]+";"+str(A)+";"+str(R)+"\n")
+             ######## Update W
+             if _c.b_train:
+                 Wold=np.c_[Wc,We] # store old Ws
+                 Wc,We=updateW()
+                 dW=np.c_[Wc,We] - Wold
+                 print "dW norm = ", np.linalg.norm(dW)
+             S=copy.copy(Snew)
+             Q=copy.copy(Qnew)
+             A=Anew
+             lastPhase=curPhase
+             upd_dur=0
+             R=0
+             D=0
+         perf_lane=np.vstack((perf_lane,s_q))
+         traci.simulationStep()
+         g_elps+=1
+         t+=1
+         
+         # Output stuffs every since a while
+         if t % 1800 ==0:
+             fout.write("\nt = "+str(t)+"\nWe = \n"+str(We)+"\n")
+             fout.write("\nWc = \n"+str(Wc)+"\n\n")
              
-             traci.simulationStep()
-             g_elps+=1
-             
-        if _c.b_outrlt and _c.b_train:
-            frlt.write(str(w))
-            w.dump('training_result')
-            frlt.close()
-        if _c.b_outPerf:
-            np.savetxt("sim_perf.csv", perf_lane,fmt='%03d', delimiter=",")
-        stop = timeit.default_timer()
-        print "computing time = ", stop-start
-        traci.close()
-    except:
-        if _c.b_outPerf:
-            np.savetxt("sim_perf.csv", perf_lane, fmt ='%03d', delimiter=",")
-   
+    if _c.b_outrlt and _c.b_train:
+        Wc.dump('Training_rslt_Wc')
+        We.dump('Training_rslt_We')
+    if _c.b_outPerf:
+        np.savetxt("sim_perf.csv", perf_lane,fmt='%03d', delimiter=",")
+    stop = timeit.default_timer()
+    print "computing time = ", stop-start
+    traci.close()
+    fout.close()
--- _c.py
+++ _c.py
@@ -4,11 +4,22 @@ User-defined parameters for the TD algorithm
 """
 # Training or simulation
 b_train = True
+# File i/o settings
+b_outlog=False
+b_outPerf=True # output performance through simulation(delay)
+b_outrlt=True # output the finaled training result (weight w)
 
-# Learning parameters
-alpha,beta=[0.0001,0.9] # parameters for updating w
-updt_freq=20 # freq of updating signal phase (and value approximator)
-T=150000 # total steps
+# Learning parameters (parameters for updating w)
+beta=0.9 # discounting rate 
+alpha_max=0.0000001 # learning rate 
+alpha_min=0.00000000000001
+alpha_maxite=1000000.0
+updt_freq=2 # freq of updating signal phase (and value approximator)
+T=9999999999999999 # total steps
+
+# OBJ parameters
+tol_er = 15 # the time length that penalty doesnt occur
+pen_rate = 0.5 # slope (assume penaly incresing linearly with time)
 
 # Updating mathod for weight w
 #updateScheme="SARSA"
@@ -18,9 +29,6 @@ updateScheme="Q"
 # decent e rate, e = max (min_p,max_p-t/max_ite)
 # thus e = min_p when t =(maxp-minp)*maxite
 e_max_p=0.8 # initial e
-e_min_p=0.05 # minimum e
-e_max_ite=100000.0
+e_min_p=0.0 # minimum e
+e_max_ite=1000000.0
 
-# File i/o settings
-b_outlog=False
-b_outrlt=True # output the finaled training result (weight w)
